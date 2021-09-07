494efbe0e79bb9d88d67b10813d7a51148841b6d
Ludvig Blomkvist <me@ludblom.net>
2021-09-07 17:03:38 +0200

    Gitignore and setup file

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 class Matrix:
-    def __init__(self, dim=):
+    def __init__(self, dim=0):
         super().__init__()
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+setup(name='CircMod',
+      version='0.1',
+      license="GPL-3",
+      author='Ludvig Blomkvist',
+      author_email='me@ludblom.net',
+      packages=find_packages(include=[])
+      )
