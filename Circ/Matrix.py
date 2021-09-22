3fa625a1eb7cb5e61ea21a6717a40e57244e9efe
ludblom <me@ludblom.net>
2021-09-22 19:14:02 +0200

    Still problems with get_Bx

--- Circ/Matrix.py
+++ Circ/Matrix.py
@@ -21,6 +21,13 @@ class HiddenSum:
         Bex = []
         Bo_size = len(self.Bo)
 
+        zeroMatrix = []
+        for j in range(Bo_size):
+            zeroMatrix.append([0 for k in range(self.k)])
+
+        # First Bex should only be zeros
+        Bex.append(zeroMatrix)
+
         # Generate the different Bex
         for i in range(Bo_size):
             tmp = []
@@ -28,23 +35,13 @@ class HiddenSum:
                 tmp.append(self.int_to_binary(self.Bo[j][i], self.k))
             Bex.append(tmp)
 
-        # The last Bex should only have zeros
-        tmp = []
-        for j in range(Bo_size):
-            tmp.append([0 for k in range(self.k)])
-        Bex.append(tmp)
+        Bex.append(zeroMatrix)
 
         return Bex
 
-    # TODO 1 is 0 for me, have to figure out a good way to convert it
     def get_Bx(self, n):
         I = self.get_identity(self.N)
-        if(n > self.N-self.k):
-            Bx = self.Bex[0]
-            for i in range(1, len(self.Bex)):
-                Bx = self.tmp_sum(Bx, self.Bex[i])
-        else:
-            Bx = self.Bex[n-1]
+        Bx = self.Bex[n%len(self.Bex)]
         for i in range(len(Bx)):
             for j in range(len(Bx[0])-1, -1, -1):
                 I[i][self.N-self.k + j] = Bx[i][j]
@@ -122,97 +119,22 @@ class HiddenSum:
     def ring(self, a, b):
         a_m = self.int_to_binary(a, self.N)
         b_m = self.int_to_binary(b, self.N)
-        print("a_m: {}, b_m: {}".format(a_m, b_m))
+        #print("a_m: {}, b_m: {}".format(a_m, b_m))
         Mx = self.get_Bx(b)
-        print("Mx: {}".format(Mx))
+        #print("a = {}, b = {}, Mx: {}".format(a, b, Mx))
         aM_b = self.matrix_mul(a_m, Mx)
-        print("aM_b: {}".format(aM_b))
+        #print("aM_b: {}".format(aM_b))
         aM_bb = self.matrix_sum(aM_b, b_m)
-        print("aM_bb: {}".format(aM_bb))
+        #print(aM_bb)
+        #print("aM_bb: {}".format(aM_bb))
+        #print("Bex: {}, Mx: {}, aM_b: {}, aM_bb: {}".format(self.Bex, Mx, aM_b, aM_bb))
         done = aM_bb
         return done
 
-
-
-
-# a = Matrix(M=[3,2])
-# b = Matrix(M=[2,2])
-# c = a.circ(b)
-
-
-
-# def print_matrix(M):
-#     for j in M:
-#         for i in j:
-#             print(i, end=' ')
-#         print()
-#     print()
-
-
-# def get_identity(n):
-#     I = []
-#     one = 0
-#     for j in range(n):
-#         I_i = []
-#         for i in range(n):
-#             if one == i:
-#                 I_i.append(1)
-#             else:
-#                 I_i.append(0)
-#         one += 1
-#         I.append(I_i)
-#     return I
-
-
-# def sum_matrix(x, y):
-#     #print("X: {}".format(x))
-#     #print("Y: {}".format(y))
-#     sum = 0
-#     for i in range(len(x)-1, 0, -1):
-#         sum = sum + x[i] + y
-#         sum = sum%2
-#     return sum
-
-
-# # TODO Do not convert x to a binary list
-# def mul_matrix(x, M):
-#     val = []
-
-#     for i in range(len(M)):
-#         tmp = []
-#         for j in range(len(M)):
-#             tmp.append(x & M[j][i])
-
-#         val.append(sum(tmp)%2)
-#     return val
-
-
-# def conv_binary(x):
-#     num = [0 for i in range(4)]
-#     while(True):
-#         if x == 0:
-#             num.append(x%2)
-
-
-
-# def calc_vals():
-#     M_1 = [[1,0,0,1],[0,1,1,0],[0,0,1,0],[0,0,0,1]]
-#     M_2 = [[1,0,1,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
-#     M_3 = get_identity(4)
-#     M_n = [[1,0,1,1],[0,1,1,0],[0,0,1,0],[0,0,0,1]]
-
-#     M = []
-#     M.append(M_1)
-#     M.append(M_2)
-#     M.append(M_3)
-#     for i in range(3, 16):
-#         M.append(M_n)
-
-#     vals = []
-
-#     for y in range(16):
-#         vals_tmp = []
-#         for x in range(16):
-#             vals_tmp.append(sum_matrix(mul_matrix(conv_binary(x), M[x]), y))
-#         vals.append(vals_tmp)
-#     print(vals)
+    def xor(self, a, b):
+        ab = []
+        a_m = self.int_to_binary(a, self.N)
+        b_m = self.int_to_binary(b, self.N)
+        for i in range(len(a_m)):
+            ab.append(a_m[i]^b_m[i])
+        return self.binary_to_int(ab)
--- test.py
+++ test.py
@@ -6,8 +6,19 @@ from Circ.Matrix import HiddenSum
 def test():
     c = HiddenSum(N=3,k=1)
 
-    for i in range(1,8):
-        print("{},{}".format(i,i))
-        print("{}".format(c.ring(i,i)))
+    for i in range(8):
+        for j in range(8):
+            #if i == 5 and j == 5:
+                #c.binary_to_int(c.ring(i,j))
+            print("{}".format(c.binary_to_int(c.ring(i,j))), end=' ')
+        print()
+    #print(c.Bex)
+    # print()
+    # for i in range(8):
+    #     for j in range(8):
+    #         #if i == 5 and j == 5:
+    #             #c.binary_to_int(c.ring(i,j))
+    #         print("{}".format(c.xor(i,j)), end=' ')
+    #     print()
 
 test()
